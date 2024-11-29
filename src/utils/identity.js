b849f2a15aa0a997775561c800c66d73c61a119e
thanhit08 <toisekhongyeu.tran@gmail.com>
2024-11-29 17:50:13 +0900

    Random an avatar from featured avatar list

--- src/utils/identity.js
+++ src/utils/identity.js
@@ -98,7 +98,7 @@ export function generateRandomName() {
 }
 
 export async function fetchRandomDefaultAvatarId() {
-  const defaultAvatarEndpoint = "/api/v1/media/search?filter=default&source=avatar_listings";
+  const defaultAvatarEndpoint = "/api/v1/media/search?filter=featured&source=avatar_listings";
   const defaultAvatars = (await fetchReticulumAuthenticated(defaultAvatarEndpoint)).entries || [];
   if (defaultAvatars.length === 0) {
     // If reticulum doesn't return any default avatars, just default to the duck model. This should only happen
