a062010943ee0e67baed71fbf105cdfd96817877
thanhit08 <toisekhongyeu.tran@gmail.com>
2024-12-16 15:09:50 +0900

    refactor file

--- src/components/npc-communication.js
+++ src/components/npc-communication.js
@@ -2,82 +2,117 @@ import { createUIButton } from "../tfl-libs/tfl-button";
 import { addObject3DComponent } from "../utils/jsx-entity";
 import { CursorRaycastable, RemoteHoverTarget, SingleActionButton } from "../bit-components";
 import { Interacted, voiceButtonData } from "../bit-components";
-import {hasComponent, addComponent, addEntity} from "bitecs";
+import { hasComponent, addComponent, addEntity } from "bitecs";
 
-AFRAME.registerComponent('npc-communication', {
+AFRAME.registerComponent("npc-communication", {
     schema: {
-        height: { type: 'number', default: 1.5 },
-        width: { type: 'number', default: 0.5 },
+        height: { type: "number", default: 1.5 },
+        width: { type: "number", default: 0.5 },
     },
 
     init: function () {
-        this.createUI("Record");
+        this.eid = null;
+        // this.audioMap = {}; // Map to store audio objects
+        // this.loadAudioFiles(); // Preload audio files
+        this.createOrUpdateUI("Record");
     },
 
-    clicked: function(world, entity) {
+    // Preload audio files
+    loadAudioFiles: function () {
+        const audioLoader = new THREE.AudioLoader();
+        const listener = new THREE.AudioListener();
+        this.el.object3D.add(listener);
+
+        this.audioMap.startRecording = new THREE.Audio(listener);
+        this.audioMap.stopRecording = new THREE.Audio(listener);
+
+        audioLoader.load("path-to-start-recording.mp3", (buffer) => {
+            this.audioMap.startRecording.setBuffer(buffer);
+        });
+
+        audioLoader.load("path-to-stop-recording.mp3", (buffer) => {
+            this.audioMap.stopRecording.setBuffer(buffer);
+        });
+    },
+
+    // Play audio by key
+    playAudio: function (key) {
+        if (this.audioMap[key] && !this.audioMap[key].isPlaying) {
+            this.audioMap[key].play();
+        }
+    },
+
+    // Stop all playing audio
+    stopAllAudio: function () {
+        Object.values(this.audioMap).forEach((audio) => {
+            if (audio.isPlaying) {
+                audio.stop();
+            }
+        });
+    },
+
+    clicked: function (world, entity) {
         return hasComponent(world, Interacted, entity);
     },
 
-    createUI: function (buttonText) {
-        console.log('Creating UI');
-
-        const btn_width = 0.5;
-        const btn_height = 0.5;
-        const text_color = "#000000";
-        const bg_color = "Play Button";
-        const font_size = 16;
-        const font = "Arial";
+    createOrUpdateUI: function (buttonText) {
+        const BUTTON_CONFIG = {
+            width: 0.5,
+            height: 0.5,
+            backgroundColor: "Play Button",
+            textColor: "#000000",
+            fontSize: 16,
+            font: "Arial",
+        };
 
         this.eid = addEntity(APP.world);
-        const voiceButton = createUIButton({
-            width: btn_width,
-            height: btn_height,
-            backgroundColor: bg_color,
-            textColor: text_color,
+
+        // Create button UI
+        this.voiceButton = createUIButton({
+            ...BUTTON_CONFIG,
             text: buttonText,
-            fontSize: font_size,
-            font: font,
         });
 
-        addObject3DComponent(APP.world, this.eid, voiceButton);
+        // Add object3D and ECS components
+        addObject3DComponent(APP.world, this.eid, this.voiceButton);
         addComponent(APP.world, voiceButtonData, this.eid);
-        if (buttonText === "Record") {
-            voiceButtonData.clicked[this.eid] = APP.getSid("false");
-        } else {
-            voiceButtonData.clicked[this.eid] = APP.getSid("true");
-        }
-        // Add mouse events to the mesh
         addComponent(APP.world, CursorRaycastable, this.eid); // Raycast
         addComponent(APP.world, RemoteHoverTarget, this.eid); // Hover
         addComponent(APP.world, SingleActionButton, this.eid); // Click
-        this.el.object3D.add(voiceButton);
+
+        // Attach the button to the entity
+        this.el.object3D.add(this.voiceButton);
+        voiceButtonData.clicked[this.eid] = APP.getSid(buttonText === "Record" ? "false" : "true");
     },
 
     tick: function () {
-        // Check clicked state
         if (this.clicked(APP.world, this.eid)) {
-            const clicked = APP.getString(voiceButtonData.clicked[this.eid]);
-            // Get object 3D component from the entity
+            const clickedState = APP.getString(voiceButtonData.clicked[this.eid]);
+            const buttonText = clickedState === "true" ? "Record" : "Recording";
+
+            // if (clickedState === "true") {
+            //     this.playAudio("stopRecording");
+            // } else {
+            //     this.playAudio("startRecording");
+            // }
+
             const voiceButton = APP.world.eid2obj.get(this.eid);
-            if (clicked === "true") {
-                voiceButtonData.clicked[this.eid] = APP.getSid("false");
-                // Remove the object 3D component from the entity
-                this.el.object3D.remove(voiceButton);
-                // Add a new object 3D component to the entity
-                this.createUI("Record");
-            } else {
-                voiceButtonData.clicked[this.eid] = APP.getSid("true");
-                this.el.object3D.remove(voiceButton);
-                this.createUI("Recording");
-            }
-            
-        }   
+            this.el.object3D.remove(voiceButton);
+            // Add a new object 3D component to the entity
+            this.createOrUpdateUI(buttonText);
+        }
     },
 
     remove: function () {
-        // Remove the object 3D component from the entity
-        const voiceButton = APP.world.eid2obj.get(this.eid);
-        this.el.object3D.remove(voiceButton);
-        console.log('Removing UI');
-    }
+        if (this.eid) {
+            const voiceButton = APP.world.eid2obj.get(this.eid);
+            if (voiceButton) {
+                this.el.object3D.remove(voiceButton);
+                console.log("Removing UI");
+            }
+        }
+
+        // Stop all playing audio
+        // this.stopAllAudio();
+    },
 });
