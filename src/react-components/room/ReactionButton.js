c8549defb795997d10f71c93d3b5220b6e1d6cdb
thanhit08 <toisekhongyeu.tran@gmail.com>
2024-12-24 11:58:27 +0900

    Update raise hand name

--- src/react-components/room/ReactionButton.js
+++ src/react-components/room/ReactionButton.js
@@ -24,9 +24,9 @@ export function HandRaisedButton({ active, ...props }) {
       <HandRaisedIcon width="42px" height="42px" />
       <p>
         {active ? (
-          <FormattedMessage id="reaction-popover.lower-hand" defaultMessage="Lower Hand" />
+          <FormattedMessage id="reaction-popover.lower-hand" defaultMessage="손 하부" />
         ) : (
-          <FormattedMessage id="reaction-popover.raise-hand" defaultMessage="Raise Hand" />
+          <FormattedMessage id="reaction-popover.raise-hand" defaultMessage="손 들기" />
         )}
       </p>
     </ReactionButton>
