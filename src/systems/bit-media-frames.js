25b7900f13d9adc8de0e12ac23dc40ffc54cdcd0
thanhit08 <toisekhongyeu.tran@gmail.com>
2025-01-17 16:25:57 +0900

    Enable snap action for media frames when captured by another user

--- src/systems/bit-media-frames.js
+++ src/systems/bit-media-frames.js
@@ -381,7 +381,7 @@ export function mediaFramesSystem(world, physicsSystem) {
     }
 
     if (MediaFrame.flags[frame] & MEDIA_FRAME_FLAGS.ACTIVE) {
-      // triggerSnapAction(frame, "off");
+      triggerSnapAction(frame, "off");
       if (capturedEid && isCapturedOwned && !isCapturedHeld && !isFrameDeleting && isCapturedColliding) {
         triggerSnapAction(frame, "on");
         snapToFrame(world, frame, capturedEid);
@@ -422,11 +422,11 @@ export function mediaFramesSystem(world, physicsSystem) {
           triggerSnapAction(frame, "off");
         }
       }
-      
+
       // Entity is captured in the frame by another user
-      // if (capturedEid && !isCapturedOwned && !isCapturedHeld && !isFrameOwned) {
-      //   triggerSnapAction(frame, "on");
-      // }
+      if (capturedEid && !isCapturedOwned && !isCapturedHeld && !isFrameOwned) {
+        triggerSnapAction(frame, "on");
+      }
     }
 
     if (
