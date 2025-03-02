fbb39f84c493d4a46d21d63da993cebf4af88faa
thanhit08 <toisekhongyeu.tran@gmail.com>
2025-03-02 19:33:34 +0900

    Add NPC button creation function with dynamic image rendering

--- src/tfl-libs/tfl-button.ts
+++ src/tfl-libs/tfl-button.ts
@@ -12,6 +12,55 @@ interface CreateUIButtonOptions {
   font: string;
 }
 
+export function createNPCButton(options: CreateUIButtonOptions): THREE.Mesh {
+  const { width, height, backgroundColor, textColor, text, fontSize, font } = options;
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d')!;
+  // Function to set the canvas size and draw the image
+  const drawImage = (imageId: string, canvasWidth: number, canvasHeight: number) => {
+    const image = document.getElementById(imageId) as HTMLImageElement;
+    if (image instanceof HTMLImageElement) {
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    }
+  };
+
+  switch (text) {
+    case "idle":
+      drawImage("npc_idle", 512, 512);
+      break;
+    case "send":
+      drawImage("npc_send", 512, 512);
+      break;
+    case "wait":
+      drawImage("npc_wait", 512, 512);
+      break;
+    case "speak":
+      drawImage("npc_speak", 512, 512);
+      break;
+    default:
+      console.log("No matching image for the text provided.");
+  }
+
+  // Create texture from canvas
+  const texture = new THREE.Texture(canvas);
+  texture.needsUpdate = true;
+
+  const transparent = true;
+
+  // Create material with texture
+  const materialParams = { map: texture, side: THREE.DoubleSide, transparent: transparent };
+  const material = new THREE.MeshBasicMaterial(materialParams);
+
+  // Create geometry
+  const geometry = new THREE.PlaneGeometry(width, height);
+
+  // Create mesh
+  return new THREE.Mesh(geometry, material);
+}
+
+
 export function createUIButton(options: CreateUIButtonOptions): THREE.Mesh {
   const { width, height, backgroundColor, textColor, text, fontSize, font } = options;
   const canvas = document.createElement('canvas');
