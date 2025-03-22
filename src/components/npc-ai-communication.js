4d6728bf21483d4be947460a88636b6049f2bd4a
thanhit08 <toisekhongyeu.tran@gmail.com>
2025-03-22 11:58:46 +0900

    Enhance NPC AI communication with animation handling and timeout fetch logic

--- src/components/npc-ai-communication.js
+++ src/components/npc-ai-communication.js
@@ -3,6 +3,7 @@ import { addObject3DComponent } from "../utils/jsx-entity";
 import { CursorRaycastable, RemoteHoverTarget, SingleActionButton } from "../bit-components";
 import { Interacted, voiceButtonData } from "../bit-components";
 import { hasComponent, addComponent, addEntity } from "bitecs";
+import { findAncestorWithComponent } from "../utils/scene-graph";
 
 AFRAME.registerComponent("npc-ai-communication", {
     schema: {
@@ -19,6 +20,11 @@ AFRAME.registerComponent("npc-ai-communication", {
         this.toggleRecording();
         this.shouldTalk = true;
         this.currentText = "idle";
+        this.waitingAnimationName = "idle";
+        this.thinkingAnimationName = "think";
+        this.answerAnimationName = "talk";
+        // this.thinkingAnimationName = "texting";
+        // this.answerAnimationName = "talking";
     },
 
     clicked: function (world, entity) {
@@ -107,17 +113,57 @@ AFRAME.registerComponent("npc-ai-communication", {
             document.getElementById('audioInput').files = dataTransfer.files;
             await this.voiceProcess();
         };
+        if (!this.mixerEl) {
+            const environmentScene = document.querySelector("#environment-scene");
+            const animationEntity = environmentScene.children[0];
+            // convert the animationEntity to el
+            const animationEl = animationEntity.object3D.el;
+            this.mixerEl = findAncestorWithComponent(animationEl, "animation-mixer");
+            if (!this.mixerEl) {
+                this.stopNPCSection();
+                console.error("No animation mixer found");
+                return;
+            }
+        }
+        this.playAnimation(this.mixerEl, this.waitingAnimationName, "stop", "npc");
+        this.playAnimation(this.mixerEl, this.thinkingAnimationName, "loop", "npc");
+    },
 
+    stopNPCSection: function () {
+        this.playAnimation(this.mixerEl, this.answerAnimationName, "stop", "npc");
+        this.playAnimation(this.mixerEl, this.thinkingAnimationName, "stop", "npc");
+        this.playAnimation(this.mixerEl, this.waitingAnimationName, "loop", "npc");
+        const npcAudio = document.getElementById('npcAudio');
+        npcAudio.pause();
+        const voiceButton = APP.world.eid2obj.get(this.eid);
+        this.el.object3D.remove(voiceButton);
+        this.createOrUpdateUI("idle");
+    },
+
+    async fetchWithTimeout(resource, options = {}) {
+        const { timeout = 8000 } = options;
+
+        const controller = new AbortController();
+        const id = setTimeout(() => controller.abort(), timeout);
+
+        const response = await fetch(resource, {
+            ...options,
+            signal: controller.signal
+        });
+        clearTimeout(id);
+
+        return response;
     },
 
     voiceProcess: async function () {
         // Delay 2 seconds to ensure the recording is saved
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        await new Promise((resolve) => setTimeout(resolve, 4000));
 
         const fileInput = document.getElementById('audioInput');
 
         if (!fileInput.files.length) {
-            alert('Please select an audio file first!');
+            this.stopNPCSection();
+            console.error('No file selected');
             return;
         }
         const file = fileInput.files[0];
@@ -130,62 +176,136 @@ AFRAME.registerComponent("npc-ai-communication", {
             // Using AI API
             const apiUrl = this.data.api;
             console.log('API URL:', apiUrl);
-            const response = await fetch(apiUrl, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                },
-                body: transcriptionFormData,
-            });
+            try {
+                const response = await fetch(apiUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                    },
+                    body: transcriptionFormData,
+                });
 
-            if (response.ok && this.shouldTalk) {
-                const voiceButton = APP.world.eid2obj.get(this.eid);
-                this.el.object3D.remove(voiceButton);
-                this.createOrUpdateUI("speak");
-                try {
-                    const responseJSON = await response.json();
-                    console.log('Audio blob:', responseJSON["blob"]);
-                    const base64Audio = responseJSON["blob"];
-                    // Remove the "data:audio/mpeg;base64," prefix to get the pure Base64 string
-                    const base64String = base64Audio.split(",")[1];
-
-                    // Convert Base64 to binary data
-                    const binaryData = atob(base64String);
-                    const byteArray = new Uint8Array(binaryData.length);
-                    for (let i = 0; i < binaryData.length; i++) {
-                        byteArray[i] = binaryData.charCodeAt(i);
-                    }
-
-                    // Create a Blob from the binary data
-                    const audioBlob = new Blob([byteArray], { type: "audio/mpeg" });
-
-                    // Create an Object URL for the Blob
-                    const audioUrl = URL.createObjectURL(audioBlob);
-                    npcAudio.src = audioUrl;
-                    npcAudio.play();
-                    npcAudio.onended = () => {
-                        const voiceButton = APP.world.eid2obj.get(this.eid);
-                        this.el.object3D.remove(voiceButton);
-                        this.createOrUpdateUI("idle");
-                    };
-                } catch (error) {
+                if (response.ok && this.shouldTalk) {
+                    const npcAudio = document.getElementById('npcAudio');
                     const voiceButton = APP.world.eid2obj.get(this.eid);
                     this.el.object3D.remove(voiceButton);
-                    this.createOrUpdateUI("idle");
-                    console.error('Error playing audio:', error);
+                    this.createOrUpdateUI("speak");
+                    try {
+                        this.playAnimation(this.mixerEl, this.thinkingAnimationName, "stop", "npc");
+                        this.playAnimation(this.mixerEl, this.answerAnimationName, "loop", "npc");
+                        const responseJSON = await response.json();
+                        console.log('Audio blob:', responseJSON["blob"]);
+                        const base64Audio = responseJSON["blob"];
+                        // Remove the "data:audio/mpeg;base64," prefix to get the pure Base64 string
+                        const base64String = base64Audio.split(",")[1];
+
+                        // Convert Base64 to binary data
+                        const binaryData = atob(base64String);
+                        const byteArray = new Uint8Array(binaryData.length);
+                        for (let i = 0; i < binaryData.length; i++) {
+                            byteArray[i] = binaryData.charCodeAt(i);
+                        }
+
+                        // Create a Blob from the binary data
+                        const audioBlob = new Blob([byteArray], { type: "audio/mpeg" });
+
+                        // Create an Object URL for the Blob
+                        const audioUrl = URL.createObjectURL(audioBlob);
+                        npcAudio.src = audioUrl;
+                        npcAudio.play();
+                        npcAudio.onended = () => {
+                            this.stopNPCSection();
+                            console.log('Audio ended');
+                        };
+                    } catch (error) {
+                        this.stopNPCSection();
+                        console.error('Error playing audio:', error);
+                        return;
+                    }
+                } else {
+                    this.stopNPCSection();
+                    console.error('Error in TTS API:', response.status, response);
+                    return;
                 }
-            } else {
-                npcAudio.pause();
-                console.error('Error in TTS API:', response.status, response);
-                const voiceButton = APP.world.eid2obj.get(this.eid);
-                this.el.object3D.remove(voiceButton);
-                this.createOrUpdateUI("idle");
+            } catch (error) {
+                this.stopNPCSection();
+                console.error('Timeout in TTS API:', error);
+                return;
             }
         } catch (error) {
+            this.stopNPCSection();
             console.error('Error converting audio to text:', error);
-            const voiceButton = APP.world.eid2obj.get(this.eid);
-            this.el.object3D.remove(voiceButton);
-            this.createOrUpdateUI("idle");
+            return;
+        }
+    },
+
+    playAnimation(mixerEl, animationName, animationType, targetClassName) {
+        const { mixer, animations } = mixerEl.components["animation-mixer"];
+
+        if (!mixer) {
+            return;
+        }
+
+        if (!animations) {
+            return;
+        }
+
+        if (!targetClassName || targetClassName === "") {
+            return;
+        }
+
+        if (!animationName || animationName === "") {
+            return;
+        }
+
+        if (!animationType || animationType === "") {
+            return;
+        }
+
+        // Try to start the animation on the robot object instead of all the objects
+        const targetObject = document.getElementsByClassName(targetClassName)[0];
+        if (!targetObject) {
+            return;
+        }
+        // Get all clip names from the loop-animation component on the robot object
+        const targetObjectLoopAnimation = findAncestorWithComponent(targetObject, "loop-animation");
+        const targetObjectLoopAnimationComponent = targetObjectLoopAnimation.components["loop-animation"];
+        if (!targetObjectLoopAnimationComponent) {
+            return;
+        }
+        const clipNames = targetObjectLoopAnimationComponent.data.allClipNames;
+        if (!clipNames) {
+            return;
+        }
+        const clipIndices = targetObjectLoopAnimationComponent.data.allClipIndices;
+        if (!clipIndices) {
+            return;
+        }
+        // Index of the animation to play
+        let animationIndex = -1;
+        for (let i = 0; i < clipNames.length; i++) {
+            if (clipNames[i] === animationName) {
+                animationIndex = i;
+                break;
+            }
+        }
+        if (animationIndex === -1) {
+            return;
+        }
+        const clipAction = mixer.clipAction(animations[clipIndices[animationIndex]]);
+
+        if (animationType === "play") {
+            clipAction.reset();
+            clipAction.setLoop(THREE.LoopOnce, 1);
+            clipAction.play();
+        }
+        if (animationType === "stop") {
+            clipAction.stop();
+        }
+        if (animationType === "loop") {
+            clipAction.reset();
+            clipAction.setLoop(THREE.LoopRepeat, Infinity);
+            clipAction.play();
         }
     },
 
